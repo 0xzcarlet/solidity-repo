2e3a5717e95041a94a7b6a0e8f2dfad11e2e9c4a
zulfikar <storyciks@gmail.com>
2025-03-07 15:20:10 +0700

    firs commit

---
+++ hackquest/phase3/function_parameter.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.21;
+// SPDX-License-Identifier: MIT 
+
+contract FunctionParameter {
+    function check(uint256 param) public pure returns (bool) {
+        return param == 0x1337;
+    }
+}
+
+contract solidityW{
+    
+}
+
+   
